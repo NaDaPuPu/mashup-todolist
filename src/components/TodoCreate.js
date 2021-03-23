432afd1e8e48518081c43ed4cc880f2b90a9e7e4
NaDaPuPu <jun19998@gmail.com>
2021-03-23 15:01:05 +0900

    Complete

--- src/components/TodoCreate.js
+++ src/components/TodoCreate.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import styled, { css } from 'styled-components';
 import { MdAdd } from 'react-icons/md';
+import { useTodoDispatch, useTodoNextId } from './../TodoContext';
 
 const CircleButton = styled.button`
   background: #38d9a9;
@@ -75,17 +76,38 @@ const Input = styled.input`
 
 function TodoCreate() {
   const [open, setOpen] = useState(false);
+  const [value, setValue] = useState('');
+
+  const dispatch = useTodoDispatch();
+  const nextId = useTodoNextId();
 
   const onToggle = () => setOpen(!open);
+  const onChange = (e) => setValue(e.target.value);
+  const onSummit = (e) => {
+    e.preventDefault(); // 새로고침 방지
+    dispatch({
+      type: 'CREATE',
+      todo: {
+        id: nextId.current,
+        text: value,
+        done: false,
+      },
+    });
+    setValue('');
+    setOpen(false);
+    nextId.current += 1;
+  };
 
   return (
     <>
       {open && (
         <InsertFormPositioner>
-          <InsertForm>
+          <InsertForm onSubmit={onSummit}>
             <Input
               autoFocus
               placeholder='할 일을 입력 후, Enter를 눌러주세요'
+              onChange={onChange}
+              value={value}
             ></Input>
           </InsertForm>
         </InsertFormPositioner>
@@ -97,4 +119,4 @@ function TodoCreate() {
   );
 }
 
-export default TodoCreate;
+export default React.memo(TodoCreate);
